37a726db44a4256e77e80839932d629b8f00fc5d
luke-dr-23 <145799428+luke-dr-23@users.noreply.github.com>
2023-11-07 16:40:33 -0500

    Add files via upload

---
+++ exit-ticket-11-7.py
@@ -0,0 +1,20 @@
+from typing import List
+# QUESTION 1
+# Write the terminal command to clone the repository with this URL: "https://github.com/JimmieLB/My-Repo.git"
+
+# QUESTION 2
+# I have some files I've been working on, and want to put them in a staging area before I'm ready to fully upload them.
+# Which GitHub command will add some number of files to a staging area?
+
+# QUESTION 3
+# What's the purpose of a README.md file?
+
+
+# CHALLENGE QUESTION
+# This will not count against your grade, so give it your best go!
+
+# Write a function that takes a list and returns a new list with only the distinct elements from that list.
+# For example, the input [1, 2, 3, 3, 1] should return [1, 2, 3]. Notice how all the repeats are gone.
+def remove_repeats(old_list: List[any]) -> List[any]:
+    # Erase pass and write your code here!
+    pass
