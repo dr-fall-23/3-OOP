264e5397a50e74a3a0a59277d0691e31ad48dc79
Rajesh-karumanchi <45812022+Rajesh-karumanchi@users.noreply.github.com>
2023-11-14 16:46:31 -0500

    Update exit-ticket-11-14.py

--- exit-ticket-11-14.py
+++ exit-ticket-11-14.py
@@ -22,3 +22,18 @@ cool_car = None
 # Ex. my_car.paint("green") should change the color of my_car to "green"
 # Then, change the color of cool_car to whatever your favorite color is!
 # NOTE: this method returns None!! It just "mutates" your Car object
+
+
+# CHALLENGE QUESTION
+# This will not count against your grade, so give it your best go!
+# Given a list nums, for each number find out how many other numbers are smaller than it, and return all of the answers in a list.
+# For example: [8, 1, 2, 2, 3] should return [4, 0, 1, 1, 3] since 8 has 4 numbers less than it, 1 has 0 numbers less than it, 2 has 1 number less than it, etc
+# (Hint: you will need multiple loops)
+def smallerNumbersThanCurrent(nums: List[int]) -> List[int]:
+    # Erase pass and write your code here!
+    pass
+
+
+print(smallerNumbersThanCurrent[8, 1, 2, 2, 3])        # should output [4, 0, 1, 1, 3]
+print(smallerNumbersThanCurrent[6, 5, 4, 8])            # should output [2, 1, 0, 3]
+print(smallerNumbersThanCurrent[7, 7, 7, 7])            # should output [0, 0, 0, 0]
