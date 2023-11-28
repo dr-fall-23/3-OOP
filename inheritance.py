9a96c377b30d42e752d71f7c9b01a400b51603fa
luke-dr-23 <145799428+luke-dr-23@users.noreply.github.com>
2023-11-28 11:43:27 -0500

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,20 +1,23 @@
+# Represents a motorized car with a distinct color, model, and year it was made
 class Car:
-    def __init__(self, color, model, year) -> None:
+    def __init__(self, color: str, model: str, year: int) -> None:
         self.color = color
         self.model = model
         self.year = year
 
 
+# Represents a motorized plane with a distinct color, model, year, and maximum altitude (in ft)
 class Plane:
-    def __init__(self, color, model, year, max_altitude) -> None:
+    def __init__(self, color: str, model: str, year: int, max_altitude: int) -> None:
         self.color = color
         self.model = model
         self.year = year
         self.max_altitude = max_altitude
 
 
+# Represents a boat with a color, model, year, and type of propulsion
 class Boat:
-    def __init__(self, color, model, year, propulsion) -> None:
+    def __init__(self, color: str, model: str, year: int, propulsion: str) -> None:
         self.color = color
         self.model = model
         self.year = year
