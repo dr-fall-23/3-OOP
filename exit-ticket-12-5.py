d0950394d9cea7a2f010de8379756a3760b477aa
luke-dr-23 <145799428+luke-dr-23@users.noreply.github.com>
2023-12-05 13:28:29 -0800

    Add files via upload

---
+++ exit-ticket-12-5.py
@@ -0,0 +1,16 @@
+# On Thursday we're going to be starting the final unit, on A.I. and machine learning.
+# This is a preliminary survey to see what topics we need to review/emphasize in this unit.
+
+# QUESTION 1
+# What do you know about A.I.? Explain in a few sentences.
+
+# QUESTION 2
+# Rate how comfortable you feel with math topics in general on a scale of 1-5
+
+# QUESTION 3
+# Rate how comfortable you feel with calculus on a scale of 1-5
+
+# QUESTION 4
+# Provide some feedback about Unit 2 (the racecars) specifically.
+# Did the pacing feel too fast/too slow? Were the labs/Hackathon engaging?
+# Did you understand how the topics we covered related to computer science in general?
